11c136dc3096c7cf0288a30e95c530654aa58d76
Pierry BENOIT <bpierry@hotmail.fr>
2023-03-24 10:57:19 +0000

    maj

--- src/flask-website/website.py
+++ src/flask-website/website.py
@@ -4,5 +4,66 @@ app = Flask(__name__)
 
 @app.route("/")
 def start(): 
-    return render_template('start.html')
+    return render_template('sign_in.html')
+
+@app.route("/index", methods=['POST'])
+def index():
+    if (check_sign_in()):
+        return render_template('start.html')
+    else:
+        return render_template('start.html')
+
+@app.route("/users")
+def users(): 
+    return render_template('users.html')
+
+@app.route("/list_serv_web")
+def list_serv_web(): 
+    return render_template('list_serv_web.html')
+
+@app.route("/detail_conf_serv_web")
+def detail_conf_serv_web(): 
+    return render_template('detail_conf_serv_web.html')
+
+@app.route("/ajout_serv_web")
+def ajout_serv_web(): 
+    return render_template('ajout_serv_web.html')
     
+@app.route("/suppr_serv_web")
+def suppr_serv_web(): 
+    return render_template('suppr_serv_web.html')
+
+@app.route("/list_serv_load_balancer")
+def list_serv_load_balancer(): 
+    return render_template('list_serv_load_balancer.html')
+
+@app.route("/detail_conf_serv_load_balancer")
+def detail_conf_serv_load_balancer(): 
+    return render_template('detail_conf_serv_load_balancer.html')
+
+@app.route("/ajout_serv_load_balancer")
+def ajout_serv_load_balancer(): 
+    return render_template('ajout_serv_load_balancer.html')
+    
+@app.route("/suppr_serv_load_balancer")
+def suppr_serv_load_balancer(): 
+    return render_template('suppr_serv_load_balancer.html')
+
+@app.route("/list_serv_reverse_proxy")
+def list_serv_reverse_proxy(): 
+    return render_template('list_serv_reverse_proxy.html')
+
+@app.route("/detail_conf_serv_reverse_proxy")
+def detail_conf_serv_reverse_proxy(): 
+    return render_template('detail_conf_serv_reverse_proxy.html')
+
+@app.route("/ajout_serv_reverse_proxy")
+def ajout_serv_reverse_proxy(): 
+    return render_template('ajout_serv_reverse_proxy.html')
+    
+@app.route("/suppr_serv_reverse_proxy")
+def suppr_serv_reverse_proxy(): 
+    return render_template('suppr_serv_reverse_proxy.html')
+
+def check_sign_in():
+    return True
