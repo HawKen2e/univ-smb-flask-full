fabb57110c087d4a327d43b7a05e3af31da6a674
Pierry BENOIT <bpierry@hotmail.fr>
2023-03-25 15:36:31 +0000

    maj

--- src/flask-api/db.py
+++ src/flask-api/db.py
@@ -12,3 +12,23 @@ if mydb.is_connected():
   print("Connexion à la base de données réussie.")
 else:
   print("Impossible de se connecter à la base de données.")
+
+#ajoute un utilisateur dans la db
+def add_user():
+  cursor = mydb.cursor()
+  users = ("pierry", "pierry", "benoit")
+  cursor.execute("""INSERT INTO user (login, first_name, last_name ) VALUES (%s, %s, %s)""", users)
+  mydb.commit()
+  mydb.close()
+  return ("donnees ajouté avec succes")
+
+def add_password():
+  cursor = mydb.cursor()
+  user_password = ("pierry", "benoit")
+  cursor.execute("""INSERT INTO user_pw (login_pw, password ) VALUES (%s, %s)""", user_password)
+  mydb.commit()
+  mydb.close()
+  return ("donnees ajouté avec succes")
+
+#add_user()
+#add_password()
