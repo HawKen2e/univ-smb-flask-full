78a93538d2db179b6d29c9027550352e4dd549ac
Pierry BENOIT <bpierry@hotmail.fr>
2023-03-25 18:19:17 +0000

    maj

--- src/flask-api/api.py
+++ src/flask-api/api.py
@@ -12,7 +12,26 @@ def hello():
 
 @app.route('/api/recevoir-donnees', methods=['POST'])
 def recevoir_donnees():
-    print('ca passe')
+    donnees = request.json
+    send_slq_pw_user(donnees)
     return 'je vien de api.py'
 
-print(type(mydb))
+def send_slq_pw_user(donnees):
+    login =[]
+    login.insert(0, donnees[0])
+    #login = donnees[0]
+    pw = donnees[1]
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM user_pw WHERE login_pw = %s", (login))
+    rows = cursor.fetchall()
+    print(rows)
+    result = []
+    for row in rows:
+        result.append({'login_pw': row[0], 'password': row[1]})
+    if (result[0]['password'] == pw) :
+        return True
+    else :
+        return False
+   
+
+#print(type(mydb))
