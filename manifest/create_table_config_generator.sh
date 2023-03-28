d1e827b198b8bcd26d76d03aaaf1b865e24af228
Pierry BENOIT <bpierry@hotmail.fr>
2023-03-28 12:28:26 +0000

    maj

--- manifest/create_table_config_generator.sh
+++ manifest/create_table_config_generator.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-
-sudo mysql -e "USE config_generator";
+USE config_generator;
 
 #table server_web
-sudo mysql -e  "CREATE TABLE IF NOT EXISTS server_web(
-        `server_name_web`varchar (50) not null,
-        `ip_address_serv_web` varchar (50) not null,
-        `root` varchar (50) not null,
-        `location` varchar (50) not null,
-        `error_page` varchar (50) not null,
-        PRIMARY KEY (`server_name`));"
+CREATE TABLE IF NOT EXISTS `server_web` (
+        server_name_web varchar (50) not null,
+        ip_address_serv_web varchar (50) not null,
+        root varchar (50) not null,
+        location varchar (50) not null,
+        error_page varchar (50) not null,
+        PRIMARY KEY (server_name));
 
 #table load_balancer
-sudo mysql -e  "CREATE TABLE IF NOT EXISTS load_balancer(
-        `server_name_load_balancer`varchar (50) not null,
-        `ip_address_load_balancer` varchar (50) not null;
-        `location_serv_web` varchar (50) not null,
-        PRIMARY KEY (`server_name`),
-        CONSTRAINT FK_location FOREIGN KEY (`location_serv_web`) REFERENCES server_web(`server_name_web`)));"
+CREATE TABLE IF NOT EXISTS `load_balancer` (
+        server_name_load_balancer varchar (50) not null,
+        ip_address_load_balance varchar (50) not null;
+        location_serv_web varchar (50) not null,
+        PRIMARY KEY (server_name),
+        CONSTRAINT FK_location FOREIGN KEY (location_serv_web) REFERENCES server_web(server_name_web));
 
 #table reverse_proxy
-sudo mysql -e "CREATE TABLE IF NOT EXISTS reverse_proxy(
-        `server_name_reverse_proxy`varchar (50) not null,
-        `ip_address_reverse_proxy` varchar (50) not null;
-        `proxy_bind` varchar (50) not null,
-        `proxy_pass` varchar (50) not null,
-        PRIMARY KEY (`server_name`),
-        CONSTRAINT FK_location FOREIGN KEY (`location_serv_web`) REFERENCES server_web(`server_name_web`)));"
+CREATE TABLE IF NOT EXISTS `reverse_proxy` (
+        server_name_reverse_proxy varchar (50) not null,
+        ip_address_reverse_proxy varchar (50) not null;
+        proxy_bind varchar (50) not null,
+        proxy_pass varchar (50) not null,
+        PRIMARY KEY (server_name),
+        CONSTRAINT FK_location FOREIGN KEY (location_serv_web) REFERENCES server_web(server_name_web));"
--- manifest/create_tables_identity.sh
+++ manifest/create_tables_identity.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
+use identity;
 
-sudo mysql -u root -p'passwordDbUniv01' -e "UPDATE mysql.user SET Password = PASSWORD('passwordDbUniv01') WHERE User = 'root';"
-
-sudo mysql -e "USE identity;"
-
-#create table user
-sudo mysql -e "CREATE TABLE IF NOT EXISTS user(
-        `login` varchar (50) not null,
-        `first_name` varchar (50) not null,
-        `last_name` varchar (50) not null,
-        PRIMARY KEY (`login`));"
+CREATE TABLE IF NOT EXISTS user (
+        login varchar (50) not null,
+        first_name varchar (50) not null,
+        last_name varchar (50) not null,
+        PRIMARY KEY (login));
 
 #table user_pw
-sudo mysql -e  "CREATE TABLE IF NOT EXISTS user_pw(
-        `login_pw` varchar (50),
-        `password` text not null,
-        CONSTRAINT FK_login_pw FOREIGN KEY (`login_pw`) REFERENCES user(`login`));"
+CREATE TABLE IF NOT EXISTS user_pw (
+        login_pw varchar (50),
+        password text not null,
+        CONSTRAINT FK_login_pw FOREIGN KEY (login_pw) REFERENCES user(login));
